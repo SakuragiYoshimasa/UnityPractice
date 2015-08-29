df23c4302917bf45e64bcaf6c46fb756c5573aac
櫻木善将 <ysrhsp@outlook.com>
2015-08-29 17:18:24 +0900

    basic movemnet

---
+++ Assets/Scripts/EnergyChargeArea.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnergyChargeArea : MonoBehaviour {
+
+	public Player player;
+
+	private void OnTriggerStay(Collider other){
+
+		if(other.gameObject.tag == ""){
+			player.EnergyCharge(1f);
+		}
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,15 +3,28 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-
+	[SerializeField]
 	private Vector3 position;
+	[SerializeField]
 	private Quaternion rotation;
+	[SerializeField]
 	private Rigidbody rigidBody;
+	[SerializeField]
 	private Vector3 velocity;
 
 	private float energy;
+	private bool boost;
+	private TurnModes mode;
 
+	private const float accelarate = 5.0f;
+	private const float boostAccelarate = 50.0f;
+	private const float FrontFrictionEffect = 0.99f;
+	private const float HorizontalFrictionEffect = 0.85f;
+	private const float turnSpeed = 1.0f;
 
+	private const float maxX = 300.0f;
+	private const float maxZ = 20.0f;
+	
 
 
 	void Start () {
@@ -25,29 +38,104 @@ public class Player : MonoBehaviour {
 		velocity = this.gameObject.GetComponent<Rigidbody>().velocity;
 
 		energy = 0f;
-		//---------------------------------------------------------------------
+		boost = false;
+		mode = TurnModes.None;
+
 
 	
 	}
 
 
-	// Update is called once per frame
-	void Update () {
-	
+	//---------------------------------------------------------------------
+	//Input and adjust velocity
+	//---------------------------------------------------------------------
+	private void Update () {
+
+		mode = TurnModes.None;
+		if(Input.GetKey("left")){
+			mode = TurnModes.Left;
+		}
+		if(Input.GetKey("right")){
+			mode = TurnModes.Right;
+		}
+
+		boost = false;
+		if(Input.GetKey("space")){
+			boost = true;
+			energy --;
+		}
 	}
 
 
+	//---------------------------------------------------------------------
+	//Update Physics and calulate
+	//---------------------------------------------------------------------
+	private void FixedUpdate(){
+
+		AdjustVelocity();
+	}
+
+
+	//---------------------------------------------------------------------
+	//EnergyCharge For BoostMode
+	//---------------------------------------------------------------------
 	public void EnergyCharge(float charge){
 
 		energy += charge;
+		Debug.Log(energy.ToString());
 
 	}
 
+
+	//---------------------------------------------------------------------
+	//When collide with obstacle, decrease velocity and AddForce with hilizontal power opposite to player velocity
+	//---------------------------------------------------------------------
 	public void OnCollisionEnter(Collision other){
 		if(other.gameObject.tag == "obstacle"){
 
 		}
 	}
 
+	//---------------------------------------------------------------------
+	//Adjust the  velocity
+	//---------------------------------------------------------------------
+	private void AdjustVelocity(){
 
+
+		//---------------------------------------------------------------------
+		//Front
+		//---------------------------------------------------------------------
+		if(velocity.x < maxX){
+
+			velocity.x += accelarate;
+		
+		}else{
+			 if(!boost){
+				velocity.x *= FrontFrictionEffect;
+			}else{
+				velocity.x += boostAccelarate;
+			}
+		}
+
+		//---------------------------------------------------------------------
+		//Horizontal
+		//---------------------------------------------------------------------
+		if(TurnMode.isTurning(mode)){
+
+			if(Mathf.Abs(velocity.z) < maxZ){
+				velocity.z += (float)TurnMode.GetDirection(mode) * turnSpeed;
+			}
+		}else{
+
+			velocity.z *=  HorizontalFrictionEffect;
+
+			if(Mathf.Abs(velocity.z) < 0.05f){
+				
+				velocity.z = 0f;
+			}
+		}
+
+
+		rigidBody.velocity = velocity;
+	}
 }
