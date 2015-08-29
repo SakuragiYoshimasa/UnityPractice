ade7762aa48edede45698ea2e70de6529249bfca
櫻木善将 <ysrhsp@outlook.com>
2015-08-29 15:50:38 +0900

    basic file tree

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+
+	private Vector3 position;
+	private Quaternion rotation;
+	private Rigidbody rigidBody;
+	private Vector3 velocity;
+
+	private float energy;
+
+
+
+
+	void Start () {
+
+		//---------------------------------------------------------------------
+		//Initialize Property
+		//---------------------------------------------------------------------
+		position = this.gameObject.transform.position;
+		rotation = this.gameObject.transform.rotation;
+		rigidBody = this.gameObject.GetComponent<Rigidbody>();
+		velocity = this.gameObject.GetComponent<Rigidbody>().velocity;
+
+		energy = 0f;
+		//---------------------------------------------------------------------
+
+	
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public void EnergyCharge(float charge){
+
+		energy += charge;
+
+	}
+
+	public void OnCollisionEnter(Collision other){
+		if(other.gameObject.tag == "obstacle"){
+
+		}
+	}
+
+
+}
